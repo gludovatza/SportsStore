1d75d525d6ecdfa83609fdd07dca99dc8f88654c
Gludovátz Attila <gla@inf.elte.hu>
2024-11-03 20:32:27 +0100

    Displaying the Page Links

--- SportsStore/obj/Debug/net8.0/SportsStore.AssemblyInfo.cs
+++ SportsStore/obj/Debug/net8.0/SportsStore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de6213b3d147dacdece4d52f60e5da2149bb9d50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e23f5c9fc34071862d029848f7d598d829aa205")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
