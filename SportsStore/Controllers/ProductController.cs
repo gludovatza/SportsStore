4d1b7d95652ec713f324770c4b826d0f4f488fc0
Gludovátz Attila <gla@inf.elte.hu>
2024-11-03 19:50:30 +0100

    Adding Pagination

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -6,14 +6,16 @@ namespace SportsStore.Controllers
     public class ProductController : Controller
     {
         private IStoreRepository repository;
+        public int PageSize = 4;
 
         public ProductController(IStoreRepository repo)
         {
             repository = repo;
         }
-        public IActionResult Index()
-        {
-            return View(repository.Products);
-        }
+        public ViewResult Index(int productPage = 1)
+            => View(repository.Products
+            .OrderBy(p => p.ProductID)
+            .Skip((productPage - 1) * PageSize)
+            .Take(PageSize));
     }
 }
