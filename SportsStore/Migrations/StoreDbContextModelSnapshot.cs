9089800c9d79488d6a626636a448af90392639bc
Ainozu <ainozuuu@gmail.com>
2024-12-09 15:26:23 +0100

    minden kész a cart részből.

--- SportsStore/Migrations/StoreDbContextModelSnapshot.cs
+++ SportsStore/Migrations/StoreDbContextModelSnapshot.cs
@@ -17,10 +17,55 @@ namespace SportsStore.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.10")
+                .HasAnnotation("ProductVersion", "8.0.11")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
+
+            modelBuilder.Entity("SportsStore.Models.Order", b =>
+                {
+                    b.Property<int>("orderId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("orderId"));
+
+                    b.Property<string>("City")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Country")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<bool>("GiftWrap")
+                        .HasColumnType("bit");
+
+                    b.Property<string>("Line1")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Line2")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Line3")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("State")
+                        .IsRequired()
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Zip")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.HasKey("orderId");
+
+                    b.ToTable("Orders");
+                });
 
             modelBuilder.Entity("SportsStore.Models.Product", b =>
                 {
---
+++ SportsStore/Pages/Cart.cshtml.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SportsStore.Infrastructure;
+using SportsStore.Models;
+
+namespace SportsStore.Pages
+{
+    public class CartModel : PageModel
+    {
+        private IStoreRepository repository;
+        public CartModel(IStoreRepository repo, Cart cartService)
+        {
+            repository = repo;
+            Cart = cartService;
+        }
+        public Cart Cart { get; set; }
+        public string ReturnUrl { get; set; } = "/";
+        public void OnGet(string returnUrl)
+        {
+            ReturnUrl = returnUrl ?? "/";
+            // Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        }
+        public IActionResult OnPost(long productId, string returnUrl)
+        {
+            Product? product = repository.Products
+                .FirstOrDefault(p => p.ProductID == productId);
+            if (product != null)
+            {
+                Cart.AddItem(product, 1);
+            }
+            return RedirectToPage(new { returnUrl = returnUrl });
+        }
+
+        public IActionResult OnPostRemove(long productId, string returnUrl)
+        {
+            CartLine relatedLine = Cart.Lines.First(cl =>
+                cl.Product.ProductID == productId);
+
+            Product product = relatedLine.Product;
+
+            if (relatedLine.Quantity == 1)
+            {
+                Cart.RemoveLine(product);
+            }
+            else
+            {
+                Cart.AddItem(product, -1); // ~= RemoveItem(1)
+            }
+            return RedirectToPage(new { returnUrl = returnUrl });
+        }
+    }
+}
